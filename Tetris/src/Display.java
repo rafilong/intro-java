47db41b96a3f64a986848d27b093942426137167
Rafi Long <rafilong42@gmail.com>
2014-01-23 11:04:48 -0800

    wrote drawEdges() method
    wrote the method to draw edges - however still draws edges when they
    have neighbors of the same type

--- Tetris/src/Display.java
+++ Tetris/src/Display.java
@@ -48,21 +48,24 @@ public class Display extends JComponent{
 	}
 	
 	public void paintComponent(Graphics g) {
+		//draws the image
 		g.drawImage(background, 0, 0, null);
+		//paints the tetrimos
 		paintGame(g);
 	}
 	
 	private static void paintGame(Graphics g) {
+		//runs through the grid and draws them if there is a square there
 		for (int w = 0; w < Main.gamewidth; w++) {
 			for (int h = 0; h < Main.gameheight - 2; h++) {
-				if (Grid.grid[w][h].hastet) paintTetrimo(g, 0, 0);
+				if (Grid.grid[w][h].hastet) paintTetrimo(g, w, h);
 			}
 		}
 	}
 	
 	private static void paintTetrimo(Graphics g, int x, int y) {
-		//offset of grid is 22 long, 16 high
-		g.setColor(colors[Grid.grid[x][y].type][1]);
+		//sets the color to the dark type color
+		g.setColor(colors[0][Grid.grid[x][y].type]);
 		//draws from the top-left to the bottom-right
 		g.fillRect(Main.xoffset + x*16, findY(y) - 16, 16, 16);
 		
@@ -70,7 +73,12 @@ public class Display extends JComponent{
 	}
 	
 	private static void drawEdges(Graphics g, int x, int y) {
-		
+		//sets the color to the light type color
+		g.setColor(colors[1][Grid.grid[x][y].type]);
+		g.drawLine(Main.xoffset + x*16, findY(y) - 16, Main.xoffset + x*16, findY(y) -1);
+		g.drawLine(Main.xoffset + x*16, findY(y) - 1, Main.xoffset + x*16 + 16 -1, findY(y) -1);
+		g.drawLine(Main.xoffset + x*16 + 16 -1, findY(y) -1, Main.xoffset + x*16 + 16 -1, findY(y) - 16);
+		g.drawLine(Main.xoffset + x*16 + 16 -1, findY(y) - 16, Main.xoffset + x*16, findY(y) - 16);
 	}
 		
 	public static int findY(int y) {
