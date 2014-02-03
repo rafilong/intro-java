c85dda89c75f59c94fedb24ad7bad17f5510ba86
Rafi Long <rafilong42@gmail.com>
2014-02-03 11:34:42 -0800

    wrote test cases for method 1
    test cases for Display.findY() completed

--- Tetris/src/Physics.java
+++ Tetris/src/Physics.java
@@ -1,17 +1,23 @@
-//makes blocks fall, clears lines, and checks whether there is "fusion"
+//Physics class: makes blocks fall, clears lines, and checks whether there is "fusion"
+
+//Documentation of the methods are in the method
 public class Physics {
 	
-	public static void drop() {
+	public static void dropcall() {
 		for (int x = 0; x < Main.gamewidth; x++) {
 			for (int y = 0; y < Main.gameheight; y++) {
-				if (Grid.grid[x][y].hastet && Grid.grid[x][y].active) {
-					Grid.grid[x][y].hastet = false;
-					Grid.grid[x][y].active = false;
-					
-					Grid.grid[x][y -1].hastet = true;
-					Grid.grid[x][y -1].active = true;
-				}
+				drop(x, y);
 			}
+		}
+	}
+	
+	public static void drop(int x, int y) {
+		if (Grid.grid[x][y].hastet && Grid.grid[x][y].active) {
+			Grid.grid[x][y].hastet = false;
+			Grid.grid[x][y].active = false;
+			
+			Grid.grid[x][y -1].hastet = true;
+			Grid.grid[x][y -1].active = true;
 		}
 	}
 }
