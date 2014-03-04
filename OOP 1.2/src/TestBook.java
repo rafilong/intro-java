1424410a43e6becc53c535df50756c1d6cb1f4bc
Rafi Long <rafilong42@gmail.com>
2014-03-04 11:30:25 -0800

    worked some more on 1.2

--- OOP 1.2/src/TestBook.java
+++ OOP 1.2/src/TestBook.java
@@ -7,18 +7,14 @@ public class TestBook {
 	//tests the class Book
 	public static void main(String args[]) {
 		//makes a new author for the use of making a book with the author
-		Author anAuthor = new Author("Tyler Packard", "tp@tylerpackard.com", 'm');
-		
+		Author anAuthor = new Author("Nihar Mitra", "nm@niharmitra.com", 'm');
 		//makes a new book using the above author, using the constructor with all arguments
-		Book aBook = new Book("'Javascript for Dummies'", anAuthor, 19.95, 1000);
+		Book book = new Book("'Javascript for Dummies'", anAuthor, 19.95, 1000);
 		
-		//prints all the information for aBook
-		System.out.println("Author information for " + aBook.getName() + " is " + aBook.getAuthor().getName() + " " + aBook.getAuthor().getEmail());
+		/* Testing Book getters */
+		System.out.println("Testing .getName(): " + book.getName());
 		
-		// Use an anonymous instance of Author
-		Book anotherBook = new Book("'Java for Dumbs'", new Author("Nihar Mitra", "nm@niharmitra.com", 'm'), 29.95, 888);
 		
-		//prints the book anotherBook, but because there isn't a toString() method it just prints the instance of the object
-		System.out.println(anotherBook);
+		
 	}
 }
