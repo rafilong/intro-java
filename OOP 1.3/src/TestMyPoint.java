b91a948f37e87d47d0f4c58264d3a7f04163983d
Rafi Long <rafilong42@gmail.com>
2014-03-10 11:17:36 -0700

    added comments

--- OOP 1.3/src/TestMyPoint.java
+++ OOP 1.3/src/TestMyPoint.java
@@ -8,33 +8,35 @@
 public class TestMyPoint {
  
 	public static void main(String args[]) {
+		//makes a new point using 2 arguments
 		MyPoint point = new MyPoint(2, 2);
 		
 		/* Testing MyPoint getters */
 		System.out.println("Testing MyPoint getters");
 		System.out.println();
 		
-		System.out.println("Testing .toString" + point);
-		System.out.println("Testing .getX: " + point.getX());
-		System.out.println("Testing .getY: " + point.getY());
-		System.out.println("Testing .distance (between 2, 2 and 3, 3): " + point.distance(3, 3));
+		System.out.println("Testing .toString: " + point); //tests toString()
+		System.out.println("Testing .getX: " + point.getX()); //tests getX
+		System.out.println("Testing .getY: " + point.getY()); //tests getY
+		System.out.println("Testing .distance (between 2, 2 and 3, 3): " + point.distance(3, 3)); //tests distance
 		
 		System.out.println();
 		System.out.println();
+		
 		
 		/*Testing MyPoint setters */
 		System.out.println("Testing myPoint setters");
 		System.out.println();
 		
-		System.out.println("Current XY: " + point);
+		System.out.println("Current XY: " + point); //tests setXY
 		point.setXY(3, 3);
 		System.out.println("Changed XY: " + point);
 
-		System.out.println("Current X: " + point.getX());
+		System.out.println("Current X: " + point.getX()); //tests setX
 		point.setX(4);
 		System.out.println("Changed X: " + point.getX());
 
-		System.out.println("Current Y: " + point.getY());
+		System.out.println("Current Y: " + point.getY()); //tests setY
 		point.setY(4);
 		System.out.println("Changed Y: " + point.getY());
 	}
