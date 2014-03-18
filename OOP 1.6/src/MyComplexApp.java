4528d9fd74d95862eae55dfb9f8f554715073bf3
Rafi Long <rafilong42@gmail.com>
2014-03-18 11:18:22 -0700

    Update MyComplexApp.java

--- OOP 1.6/src/MyComplexApp.java
+++ OOP 1.6/src/MyComplexApp.java
@@ -7,6 +7,21 @@ import java.util.Scanner;
 
 public class MyComplexApp {
 
+	public static void main(String args[]) {
+		System.out.print("Enter complex number 1 (real and imaginary part): ");
+		MyComplex complex = newComplex();
+		System.out.println();
+		
+		System.out.print("Enter complex number 2 (real and imaginary part): ");
+		MyComplex another = newComplex();
+		System.out.println();
+		
+		System.out.println();
+		
+		System.out.print("Number 1 is: " + complex);
+		System.out.println(complex + " is " +)
+	}
+	
 	public static String textEnter() {
 		//goes through all the steps to get a string from the scanner
 		Scanner sc = new Scanner(System.in);
@@ -14,20 +29,24 @@ public class MyComplexApp {
 		return text;
 	}
 	
-	public void interperet(String text) {		
+	public static double[] interperet(String text) {		
 		int wordstart = 0;
+		double[] complex = new double[2];
 		for (int i = 0; i <= text.length(); i++) {
 			if (i == text.length()) {
-				translate(text.substring(wordstart, i));
+				complex[1] = Double.parseDouble(text.substring(wordstart, i));
 			} else if (text.substring(i, i+1).equals(" ")) {
-				MyComplex.this.setReal(text.substring(wordstart, i));
+				complex[0] = Double.parseDouble(text.substring(wordstart, i));
 				wordstart = i+1;
 			}
 		}
+		return complex;
 	}
 	
 	public static MyComplex newComplex() {
+		String scanned = textEnter();
 		MyComplex complex = new MyComplex(0, 0);
-		complex.interperet
+		complex.setValue(interperet(scanned)[0], interperet(scanned)[1]);
+		return complex;
 	}
 }
