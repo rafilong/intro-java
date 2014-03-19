1bb894577b12e00c86137fbb7b0a849669e4e511
Rafi Long <rafilong42@gmail.com>
2014-03-19 10:57:40 -0700

    added commenting

--- OOP 1.6/src/MyComplexApp.java
+++ OOP 1.6/src/MyComplexApp.java
@@ -38,36 +38,44 @@ public class MyComplexApp {
 		MyComplex added = complex.add(another); //finds what the value of the added variables equal
 		MyComplex subtracted = complex.subtract(another); //finds what the value of the subtracted variables equal
 		
-		System.out.println(complex + " is" + trueFalse[equal] + "equal to " + another);
-		System.out.println(complex + " + " + another + " = " + added);
-		System.out.println(complex + " - " + another + " = " + subtracted);
+		System.out.println(complex + " is" + trueFalse[equal] + "equal to " + another); //uses equal and trueFalse to print whether the numbers are equal or not
+		System.out.println(complex + " + " + another + " = " + added); //prints the variables added (complex and another) and added
+		System.out.println(complex + " - " + another + " = " + subtracted); //prints the variabls subtracted (complex and another) and subtracted
 	}
 	
 	public static String textEnter() {
 		//goes through all the steps to get a string from the scanner
-		Scanner sc = new Scanner(System.in);
-		String text = sc.nextLine();
-		return text;
+		Scanner sc = new Scanner(System.in); //makes a new scanner
+		String text = sc.nextLine(); //makes the variable text equal the scanner when there is a new line
+		return text; //returns text
 	}
 	
-	public static double[] interperet(String text) {		
-		int wordstart = 0;
-		double[] complex = new double[2];
-		for (int i = 0; i <= text.length(); i++) {
-			if (i == text.length()) {
-				complex[1] = Double.parseDouble(text.substring(wordstart, i));
-			} else if (text.substring(i, i+1).equals(" ")) {
-				complex[0] = Double.parseDouble(text.substring(wordstart, i));
-				wordstart = i+1;
+	public static double[] interperet(String text) {
+		//breaks a String with multiple numbers into ints
+		try { //makes a try catch that will catch if the String entered has characters
+			int wordstart = 0; //a variable holding the start of the world
+			double[] complex = new double[2]; //a variable for holding the ints
+			for (int i = 0; i <= text.length(); i++) { //a for loop that repeats until the end of the String
+				if (i == text.length()) { //an if statement, where if the for loop has made it to the end of the String it activates
+					complex[1] = Double.parseDouble(text.substring(wordstart, i)); //adds a new number to the array between wordstart and the current space
+				} else if (text.substring(i, i+1).equals(" ")) { //an if statement that activates if there is a space
+					complex[0] = Double.parseDouble(text.substring(wordstart, i)); //adds the number between the last word and the current space
+					wordstart = i+1; //resets wordstart
+				}
 			}
+			return complex; //returns the array
+		} catch (NumberFormatException e) { //catches if the String entered has characters
+			System.out.println("Not valid type, please start over"); //asks the user to start over
+			System.exit(0); //exits the program
 		}
-		return complex;
+		return null; //a return that will never get activated, but is included to avoid errors
 	}
 	
 	public static MyComplex newComplex() {
-		String scanned = textEnter();
-		MyComplex complex = new MyComplex(0, 0);
-		complex.setValue(interperet(scanned)[0], interperet(scanned)[1]);
-		return complex;
+		//makes a new MyComplex
+		String scanned = textEnter(); //makes the string equal to a text enter
+		MyComplex complex = new MyComplex(0, 0); //makes a new MyComplex
+		complex.setValue(interperet(scanned)[0], interperet(scanned)[1]); //sets the value of MyComplex to equal the values entered using interperet()
+		return complex; //returns the complex number
 	}
 }
