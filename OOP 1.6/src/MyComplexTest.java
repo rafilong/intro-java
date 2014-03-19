8be562b638b6ab8ddcd268698d711fee1873b08c
Rafi Long <rafilong42@gmail.com>
2014-03-19 10:43:30 -0700

    added returns

--- OOP 1.6/src/MyComplexTest.java
+++ OOP 1.6/src/MyComplexTest.java
@@ -4,7 +4,10 @@
  * 
  * Documentation is in the code
  * Run main() in MyComplexTest to see testing
+ *   Expected returns are in comments near the print statement
  * Run main() in MyComplexApp to see program
+ * Description of methods are below, and all methods are in MyComplex
+ * Testing is organized in getters, setters, and returns, and are found below in the main() method
  */
 
 /*
@@ -46,9 +49,9 @@ public class MyComplexTest {
 		System.out.println("Testing getters");
 		System.out.println();
 		
-		System.out.println("Testing getReal: " + complex.getReal()); //calls getReal
-		System.out.println("Testing getImag: " + complex.getImag()); //calls getImag
-		System.out.println("Testing toString: " + complex); //implicitly calls toString
+		System.out.println("Testing getReal: " + complex.getReal()); //calls getReal | return is 2
+		System.out.println("Testing getImag: " + complex.getImag()); //calls getImag | return is 2
+		System.out.println("Testing toString: " + complex); //implicitly calls toString | return is (2, 2)
 		
 		System.out.println();
 		System.out.println();
@@ -59,45 +62,45 @@ public class MyComplexTest {
 		System.out.println();
 		
 		System.out.println("Testing setReal");
-		System.out.println("Current real: " + complex.getReal()); //finds number with getReal
+		System.out.println("Current real: " + complex.getReal()); //finds number with getReal | return is 2
 		complex.setReal(5); //changes the real with setReal
-		System.out.println("Changed radius: " + complex.getReal());  //finds number with getReal
+		System.out.println("Changed radius: " + complex.getReal());  //finds number with getReal | return is 5
 		System.out.println();
 		
 		System.out.println("Testing setImag");
-		System.out.println("Current real: " + complex.getImag()); //finds number with getImag
+		System.out.println("Current real: " + complex.getImag()); //finds number with getImag | return is 2
 		complex.setImag(5); //changes the imaginary with setImag
-		System.out.println("Changed radius: " + complex.getImag()); //finds number with getImag
+		System.out.println("Changed radius: " + complex.getImag()); //finds number with getImag | return is 5
 		System.out.println();
 
 		System.out.println("Testing setValue");
-		System.out.println("Current values: " + complex); //finds number by implicitly calling toString 
+		System.out.println("Current values: " + complex); //finds number by implicitly calling toString | return is (5, 5)
 		complex.setValue(6, 6); //changes the values with setValue
-		System.out.println("Changed radius: " + complex); //finds number by implicitly calling toString 
+		System.out.println("Changed radius: " + complex); //finds number by implicitly calling toString | return is (6, 6)
 		System.out.println();
 
 		System.out.println("Testing add");
-		System.out.println("Current values: " + complex); //finds number by implicitly calling toString 
-		complex.add(another); //adds the complex number by `another` with multiplyWith
-		System.out.println("Changed values: " + complex); //finds number by implicitly calling toString 
+		System.out.println("Current values: " + complex); //finds number by implicitly calling toString | return is (6, 6)
+		MyComple added = complex.add(another); //adds the complex number by `another` with multiplyWith
+		System.out.println("Changed values: " + added); //finds number by implicitly calling toString | return is (9, 9)
 		System.out.println();
 
 		System.out.println("Testing subtract");
-		System.out.println("Current real: " + complex); //finds number by implicitly calling toString
-		complex.subtract(another); //subtracts the complex number by `another` with divideBy
-		System.out.println("Changed values: " + complex); //finds number by implicitly calling toString
-
+		System.out.println("Current real: " + complex); //finds number by implicitly calling toString | return is (6, 6)
+		MyComplex subtracted = complex.subtract(another); //subtracts the complex number by `another` with divideBy
+		System.out.println("Changed values: " + subtracted); //finds number by implicitly calling toString | return is (3, 3)
+		System.out.println();
 		
 		System.out.println("Testing multiplyWith");
-		System.out.println("Current values: " + complex); //finds number by implicitly calling toString 
+		System.out.println("Current values: " + complex); //finds number by implicitly calling toString | return is (6, 6)
 		complex.multiplyWith(another); //multiplies the complex number by `another` with multiplyWith
-		System.out.println("Changed values: " + complex); //finds number by implicitly calling toString 
+		System.out.println("Changed values: " + complex); //finds number by implicitly calling toString | return is (0, 18)
 		System.out.println();
 
 		System.out.println("Testing divideBy");
-		System.out.println("Current real: " + complex); //finds number by implicitly calling toString
+		System.out.println("Current real: " + complex); //finds number by implicitly calling toString | return is (0, 18)
 		complex.divideBy(another); //divides the complex number by `another` with divideBy
-		System.out.println("Changed values: " + complex); //finds number by implicitly calling toString
+		System.out.println("Changed values: " + complex); //finds number by implicitly calling toString | return is (315, 34.5)
 		
 		System.out.println();
 		System.out.println();
@@ -107,11 +110,11 @@ public class MyComplexTest {
 		System.out.println("Testing returns");
 		System.out.println();
 		
-		System.out.println("Testing isReal: " + complex.isReal()); //calls isReal
-		System.out.println("Testing isImaginary: " + complex.isImaginary()); //calls isImaginary
-		System.out.println("Testing equals: " + complex.equals(another)); //calls equals
-		System.out.println("Testing magnitude: " + complex.magnitude()); //calls magnitude
-		System.out.println("Testing argumentInRadians: " + complex.argumentInRadians()); //calls argumentInRadians
-		System.out.println("Testing arguemntInDegrees: " + complex.argumentInDegrees()); //calls argumentInDegrees
+		System.out.println("Testing isReal: " + complex.isReal()); //calls isReal | return is false
+		System.out.println("Testing isImaginary: " + complex.isImaginary()); //calls isImaginary | return is true
+		System.out.println("Testing equals: " + complex.equals(another)); //calls equals | return is false
+		System.out.println("Testing magnitude: " + complex.magnitude()); //calls magnitude | return is 316.88365372798893
+		System.out.println("Testing argumentInRadians: " + complex.argumentInRadians()); //calls argumentInRadians | return is 0.10908900499998157
+		System.out.println("Testing arguemntInDegrees: " + complex.argumentInDegrees()); //calls argumentInDegrees | return is 6
 	}
 }
