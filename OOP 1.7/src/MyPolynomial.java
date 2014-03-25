f6d7fb79c11859103cd00d6c9a6f162356c6366b
Rafi Long <rafilong42@gmail.com>
2014-03-25 11:06:45 -0700

    worked on 1.7

--- OOP 1.7/src/MyPolynomial.java
+++ OOP 1.7/src/MyPolynomial.java
@@ -15,56 +15,56 @@ public class MyPolynomial {
 	}
 	
 	public MyPolynomial(String filename) {
-		Scanner in = null;
-		try {
-			in = new Scanner(new File(filename)); // open file
-		} catch (FileNotFoundException e) {
+		Scanner in = null; //sets the scanner value to equal nothing
+		try { //try catch for when the file is not in place
+			in = new Scanner(new File(filename)); // opens file
+		} catch (FileNotFoundException e) { //catches when the file isn't found
 			System.err.println("Could not find file, error found below: ");
 			e.printStackTrace();
 		}
-		int degree = in.nextInt(); // read the degree
-		coeffs = new double[degree + 1]; // allocate the array
+		int degree = in.nextInt(); // reads the degree
+		coeffs = new double[degree + 1]; // allocates the array
 		for (int i = 0; i < coeffs.length; ++i) {
-			coeffs[i] = in.nextDouble();
+			setCoeffs(i, in.nextDouble()); //sets the coeffs using setCoeffs
 		}
 	}
 	
 	public double[] getCoeffs() {
-		return this.coeffs;
+		return this.coeffs; //returns the list of coeffs
 	}
 	
 	public double getCoeffs(int place) {
-		return this.coeffs[place];
+		return this.coeffs[place]; //returns the coeffs in a certain place
 	}
 
 	public void setCoeffs(double[] coeffs) {
-		this.coeffs = coeffs;
+		this.coeffs = coeffs; //sets the coeffs with an array
 	}
 	
 	public void setCoeffs(int place, double value) {
-		this.coeffs[place] = value;
+		this.coeffs[place] = value; //sets the coeffs in a certain place
 	}
 
 	public int getDegree() {
-		return this.getCoeffs().length - 1;
+		return this.getCoeffs().length - 1; //gets the degree of the polynomial by finding the length
 	}
 	
 	public String toString() {
-		if (coeffs.length > 1) {
+		if (coeffs.length > 1) { //prints the primary coefficients (so they won't have the ^1 and ^0)
 			String retval = coeffs[1] + "x + " + coeffs[0];
-			for (int i = 2; i < this.getDegree() + 1; i++) {
+			for (int i = 2; i < this.getDegree() + 1; i++) { //a for loop that loops for all of the coefficients
 				retval = this.getCoeffs(i) + "x^" + (i) + " + " + retval; 
 			}
-			return retval;
+			return retval; //returns the retval
 		} else {
-			return coeffs[0] + "";
+			return coeffs[0] + ""; //if the polynomial is of size one, it prints the coefficient of the 0th power
 		}
 	}
 	
 	public double evaluate(double x) {
-		double retval = 0;
-		for (int i = 0; i < this.getDegree(); i++) {
-			retval += this.getCoeffs(i) * Math.pow(x, i); 
+		double retval = 0; //sets retval to equal zero
+		for (int i = 0; i <= this.getDegree(); i++) { //
+			retval += this.getCoeffs(i) * Math.pow(x, i);
 		}
 		return retval;
 	}
@@ -73,14 +73,21 @@ public class MyPolynomial {
 		double[] out;
 		if (this.getDegree() > another.getDegree()) {
 			out = new double[this.getDegree() + 1];
-			for (int i = 0; i <= another.getDegree(); i++) {
-				out[i] = this.getCoeffs(i) + another.getCoeffs(i); 
+			for (int i = 0; i <= this.getDegree(); i++) {
+				if (i > another.getDegree()) {
+					out[i] = this.getCoeffs(i); 
+				} else {
+					out[i] = this.getCoeffs(i) + another.getCoeffs(i); 
+				}
 			}
 		} else {
 			out = new double[another.getDegree() + 1];
 			for (int i = 0; i <= this.getDegree(); i++) {
-				out[i] = this.getCoeffs(i) + another.getCoeffs(i); 
-			}
+				if (i > this.getDegree()) {
+					out[i] = another.getCoeffs(i); 
+				} else {
+					out[i] = another.getCoeffs(i) + this.getCoeffs(i); 
+				}			}
 		}
 		return new MyPolynomial(out);
 	}
--- OOP 1.7/src/TestMyPolynomial.java
+++ OOP 1.7/src/TestMyPolynomial.java
@@ -3,6 +3,10 @@
  * Code written by Rafi Long
  * 
  * See code for documentation
+ * Run main() in TestMyPolynomial for test cases
+ * Expected return is next to the print statement
+ * 
+ * Setters and getters and implicitly invoked with add, multiply, getDegree, evaluate
  */
 
 public class TestMyPolynomial {
@@ -18,7 +22,10 @@ public class TestMyPolynomial {
 		System.out.println("Pola is " + pola);
 		
 		System.out.println();
-		System.out.println("Added is " + poly.add(pola));
-		System.out.println("Multi is " + poly.multiply(pola));
+		System.out.println("Testing toString: " + poly); //expected return is 3.0x^2 + 2.0x + 1.0
+		System.out.println("Testing add: " + poly.add(pola));  //expected return is 3.0x^2 + 4.0x + 2.0
+		System.out.println("Testing multiply: " + poly.multiply(pola)); //expected return is 6.0x^3 + 7.0x^2 + 4.0x + 1.0
+		System.out.println("Testing getDegree: " + poly.getDegree()); //expected return is 2
+		System.out.println("Testing evaluate: " + poly.evaluate(1)); //expected return is 6.0
 	}
 }
